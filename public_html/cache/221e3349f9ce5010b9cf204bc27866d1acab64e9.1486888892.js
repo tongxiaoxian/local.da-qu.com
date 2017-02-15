467681b8b9f250ae001c0dbbaff02b0bb23275b1
TONG XIAO XIAN <tongxiaoxian@icloud.com>
2017-02-15 14:34:45 +0800

    no message

---
+++ public_html/cache/221e3349f9ce5010b9cf204bc27866d1acab64e9.1486888892.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    document.oncontextmenu = function () { return false; }
+});
+
+$(document).ready(function() {
+	$('img').css({pointerEvents: "none"})
+
+});
+
+;
