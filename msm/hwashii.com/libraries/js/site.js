d6836ea1236d3fba117126b0879da678be987fb7
TONG XIAO XIAN <tongxiaoxian@icloud.com>
2017-04-07 11:41:27 +0800

    no message

--- msm/hwashii.com/libraries/js/site.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	var image = $('#backstretch').attr("src");
-	$("header").backstretch(image, {speed: 150});
-});
-
-$(document).ready(function() {
-	document.oncontextmenu = function () { return false; }
-});
