0e60b895431923d9a1dd13d1a6d601dafeb4372f
TONG XIAO XIAN <tongxiaoxian@icloud.com>
2017-07-04 21:07:24 +0800

    no message

--- msm/nightblindnesscafe.com/libraries/js/site.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	var image = $('#backstretch').attr("src");
-	$("main").backstretch(image, {speed: 150});
-});
-
-$(document).ready(function() {
-	document.oncontextmenu = function () { return false; }
-});
