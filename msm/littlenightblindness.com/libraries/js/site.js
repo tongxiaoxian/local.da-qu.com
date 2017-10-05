05508579ebe8e239b3230bada1ad1da4b3edddea
TONG XIAO XIAN <tongxiaoxian@icloud.com>
2017-10-05 13:39:25 +0800

    no message

--- msm/littlenightblindness.com/libraries/js/site.js
+++ msm/littlenightblindness.com/libraries/js/site.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var image = $('#backstretch').attr("src");
-	$("header").backstretch(image, {speed: 150});
+	// $("section.banner").backstretch(image, {speed: 150});
 });
 
 $(document).ready(function() {
