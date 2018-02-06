e4cef30a57b0205462b7aad9835a2565d070f68d
TONG XIAO XIAN <tongxiaoxian@icloud.com>
2018-02-06 09:56:24 +0800

    no message

---
+++ public_html/archives/storage/configuration/database.php
@@ -0,0 +1,10 @@
+<?php
+	return array(
+		'driver' => 'mysqli',
+		'hostname' => 'localhost',
+		'database' => 'dq_koken_DB',
+		'username' => 'root',
+		'password' => 'Ecopillow123=',
+		'prefix' => 'koken_',
+		'socket' => ''
+	);
